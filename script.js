0d0087276783a1891176d75257cd13d8ace19e60
LUIZ FERNANDO RIBEIRO SANTOS <luiz_santos25@estudante.sesisenai.org.br>
2025-03-05 10:15:42 -0300

    Personagem pula

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 const canvas = document.getElementById('jogo2D')
 const ctx = canvas.getContext('2d')
-function loop(){
+
+document.addEventListener('keypress', (e) => {
+    if(e.code == 'Space'){
+        personagem.velocidY =  15
+        console.log("PULOU")
+        
     
+    }})
+
+const personagem ={
+    x: 100,
+    y: canvas.height - 50,
+    altura: 50,
+    largura: 50,
+    velocidY: 0,
+    pulando: true
+}
+
+function desenharPersonagem(){
+    ctx.fillRect(personagem.x, personagem.y, personagem.altura,personagem.largura)
+    ctx.fillStyle = 'black'
+}
+function atualizarPersonagem(){
+    if(personagem.pulando == true){
+        personagem.y -= personagem.velocidY
+       
+    }
+}
+
+function loop(){
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+    desenharPersonagem()
+    atualizarPersonagem()
+    
+    requestAnimationFrame(loop)
 }
 
 loop()
