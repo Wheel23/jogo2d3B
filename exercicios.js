dc38614d261891f349c30a4cd9c4499b28c7bcb7
LUIZ FERNANDO RIBEIRO SANTOS <luiz_santos25@estudante.sesisenai.org.br>
2025-03-12 08:49:01 -0300

    Realizando atividades

---
+++ exercicios.js
@@ -0,0 +1,92 @@
+class veiculo{
+    constructor(tipo,marca,cor,velocidade,passageiros){
+        this.tipo = tipo
+        this.marca = marca
+        this.cor = cor
+        this.velocidade = velocidade
+        this.passageiros = passageiros
+    }
+    acelerar = function(){
+        this.velocidade += 10
+        console.log(this.velocidade)
+    }
+    freiar = function(){
+        this.velocidade -= 10
+        console.log(this.velocidade)
+        if(this.velocidade <= 0){
+            console.log('ESTA PARADO')
+        }
+    }
+}
+const carro = new veiculo(
+    'conversivel',
+    'Ford',
+    'Preto',
+   100,
+    2
+)
+
+console.log(carro)
+carro.acelerar()
+carro.acelerar()
+carro.freiar()
+
+class aviao extends veiculo{
+    constructor(tipo,marca,cor,velocidade,passageiros, companhia){
+        super(tipo,marca,cor,velocidade,passageiros);
+        this.companhia = companhia;
+    }
+}
+
+const Aviao = new aviao('Comercial', '19 bis', 'amarelo',0, 0, 'Goleiro')
+
+console.log(Aviao)
+
+
+class barco extends veiculo{
+    constructor(tipo,marca,cor,velocidade,passageiros, motor){
+        super(tipo,marca,cor,velocidade,passageiros);
+        this.motor = motor;
+}
+acelerar = function(){
+    this.velocidade += 10
+    console.log(this.velocidade)
+}
+freiar = function(){
+    this.velocidade -= 10
+    console.log(this.velocidade)
+    if(this.velocidade <= 0){
+        console.log('ESTA PARADO')
+    }
+}
+
+}
+const Barco = new barco('Comercial', 'TITANIO', 'amarelo', 0, 0, 'Goleiro 92')
+console.log(Barco)
+
+class jato extends veiculo{
+    constructor(tipo,marca,cor,velocidade,passageiros, motor){
+        super(tipo,marca,cor,velocidade,passageiros);
+        this.motor = motor;
+}
+acelerar = function(){
+    this.velocidade += 10
+    console.log(this.velocidade)
+}
+freiar = function(){
+    this.velocidade -= 10
+    console.log(this.velocidade)
+    if(this.velocidade <= 0){
+        console.log('ESTA PARADO')
+    }
+}
+}
+const Jato = new jato('Comercial', '19 bis', 'amarelo',0, 0, 'Goleiro')
+
+console.log(Jato)
+Barco.acelerar()
+Barco.freiar()
+Jato.acelerar()
+Jato.freiar()
+Aviao.acelerar()
+Aviao.freiar()
